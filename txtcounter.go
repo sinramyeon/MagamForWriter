41616bc683e60d34acd470d75d429e5b5852d202
kimseolhwa <esistkalt@naver.com>
2018-06-21 11:34:51 +0900

    글자수 세기 보강

--- txtcounter.go
+++ txtcounter.go
@@ -5,6 +5,22 @@ import (
 	"unicode/utf8"
 )
 
+func CountAll(s string) int{
+	return len([]rune(s))
+}
+
+func CountRemoveBlank(s string) int{
+	var cnt_blank int
+	stored_text_length := CountAll(s)
+
+	for(int i=0; i<stored_text_length; i++){
+		if(s.charAt(i) == ' ' || s.charAt(i) == '\n' || s.charAt(i) == '\t')
+			cnt_blank++;
+	}
+	exclude_blank_words := total_count_words -  cnt_blank;
+	return exclude_blank_words
+}
+
 // https://engineering.linecorp.com/ko/blog/detail/52
 func CountChar(s string) int {
 	if len(s) == 0 {
