8100626eab19f8a46d67737a2e6cb5c1991195b6
hero0926 <esistkalt@naver.com>
2018-06-27 17:33:25 +0900

    패키지화 중

--- fileupload.go
+++ fileupload.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"time"
 
@@ -321,7 +319,7 @@ func Fileupload() {
 
 							// 3. 마감일 정하기
 							txtFile.DdaySet()
-							err := saveFile(txtFile.dday, txtFile.path)
+							err := SaveFile(txtFile.dday, txtFile.path)
 							if err != nil {
 								walk.MsgBox(
 									nil,
@@ -349,77 +347,4 @@ func Fileupload() {
 	splitter.SetFixed(tableView, true)
 
 	mainWindow.Run()
-}
-
-func txtFileOpen(filepath string) string {
-	file, err := os.Open(filepath)
-	if err != nil {
-		return err.Error()
-	}
-	defer file.Close()
-
-	fi, err := file.Stat()
-	if err != nil {
-		return err.Error()
-	}
-
-	var data = make([]byte, fi.Size())
-
-	_, err = file.Read(data)
-	if err != nil {
-		return err.Error()
-	}
-
-	return string(data)
-}
-
-func saveFile(day, filepath string) error {
-	txt := day + " " + filepath + ";" //2018-06-20 C:\windows-version.txt;
-	var file, err = os.OpenFile("C:\\temp\\magamDday.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	defer file.Close()
-
-	_, err = file.WriteString(txt)
-
-	file.Sync()
-
-	return err
-}
-
-func GetAlarmText() (string, string, string, string) {
-
-	var newFile TxtFile
-	txt := getFile()
-	filearray := strings.Split(txt, ";") //2018-06-20 C:\windows-version.txt;
-
-	for i := range filearray {
-		oneFile := strings.Split(filearray[i], " ")
-		if len(oneFile) > 0 {
-
-			newFile.dday = oneFile[0]
-			newFile.name = oneFile[1]
-
-			str := txtFileOpen(newFile.name)
-			count := CountAll(str)
-			countWithoutBlank := CountRemoveBlank(str)
-			return newFile.dday, newFile.name, strconv.Itoa(count), strconv.Itoa(countWithoutBlank)
-
-		}
-	}
-	return "", "", "", ""
-}
-
-func getFile() string {
-	// 1. 파일 가져오기
-
-	var file, err = ioutil.ReadFile("C:\\temp\\magamDday.txt")
-
-	if err != nil {
-		walk.MsgBox(
-			nil,
-			"Error",
-			err.Error(),
-			walk.MsgBoxOK|walk.MsgBoxIconError)
-	}
-
-	return string(file)
 }
--- util.go
+++ util.go
@@ -1,6 +1,13 @@
 package main
 
-import "strings"
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/lxn/walk"
+)
 
 func GetFilename(filePath string) string {
 	i := strings.Index(filePath, "\\")
@@ -14,4 +21,77 @@ func GetFilename(filePath string) string {
 		return filePath
 	}
 
+}
+
+func TxtFileOpen(filepath string) string {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err.Error()
+	}
+	defer file.Close()
+
+	fi, err := file.Stat()
+	if err != nil {
+		return err.Error()
+	}
+
+	var data = make([]byte, fi.Size())
+
+	_, err = file.Read(data)
+	if err != nil {
+		return err.Error()
+	}
+
+	return string(data)
+}
+
+func SaveFile(day, filepath string) error {
+	txt := day + " " + filepath + ";" //2018-06-20 C:\windows-version.txt;
+	var file, err = os.OpenFile("C:\\temp\\magamDday.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	defer file.Close()
+
+	_, err = file.WriteString(txt)
+
+	file.Sync()
+
+	return err
+}
+
+func GetAlarmText() (string, string, string, string) {
+
+	var newFile TxtFile
+	txt := GetFile()
+	filearray := strings.Split(txt, ";") //2018-06-20 C:\windows-version.txt;
+
+	for i := range filearray {
+		oneFile := strings.Split(filearray[i], " ")
+		if len(oneFile) > 0 {
+
+			newFile.dday = oneFile[0]
+			newFile.name = oneFile[1]
+
+			str := TxtFileOpen(newFile.name)
+			count := CountAll(str)
+			countWithoutBlank := CountRemoveBlank(str)
+			return newFile.dday, newFile.name, strconv.Itoa(count), strconv.Itoa(countWithoutBlank)
+
+		}
+	}
+	return "", "", "", ""
+}
+
+func GetFile() string {
+	// 1. 파일 가져오기
+
+	var file, err = ioutil.ReadFile("C:\\temp\\magamDday.txt")
+
+	if err != nil {
+		walk.MsgBox(
+			nil,
+			"Error",
+			err.Error(),
+			walk.MsgBoxOK|walk.MsgBoxIconError)
+	}
+
+	return string(file)
 }
