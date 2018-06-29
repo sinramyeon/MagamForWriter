de2641199b25bb4937a467a31a14ccf0328b2308
hero0926 <esistkalt@naver.com>
2018-06-29 17:54:05 +0900

    actions를 적용하자 또 놀랍게도 실행이 안되고있음

--- start.go
+++ start.go
@@ -1,36 +1,201 @@
 package main
 
 import (
+	"log"
+
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 )
 
+type MyMainWindow struct {
+	*walk.MainWindow
+}
+
 func main() {
 
-	var mainWindow *walk.MainWindow
+	mw := new(MyMainWindow)
 
-	MainWindow{
-		AssignTo: &mainWindow,
-		Title:    "마감 안내기",
-		MinSize:  Size{120, 50},
-		Layout:   VBox{},
-		Children: []Widget{
+	var openAction, showAboutBoxAction *walk.Action
+	var recentMenu *walk.Menu
+	var toggleSpecialModePB *walk.PushButton
 
-			PushButton{
-				Text: "작업 등록하기",
-				OnClicked: func() {
-					mainWindow.Close()
-					Fileupload()
+	if err := (MainWindow{
+		AssignTo: &mw.MainWindow,
+		Title:    "Walk Actions Example",
+		MenuItems: []MenuItem{
+			Menu{
+				Text: "&File",
+				Items: []MenuItem{
+					Action{
+						AssignTo:    &openAction,
+						Text:        "&Open",
+						Image:       "../img/open.png",
+						Enabled:     Bind("enabledCB.Checked"),
+						Visible:     Bind("!openHiddenCB.Checked"),
+						Shortcut:    Shortcut{walk.ModControl, walk.KeyO},
+						OnTriggered: mw.openAction_Triggered,
+					},
+					Menu{
+						AssignTo: &recentMenu,
+						Text:     "Recent",
+					},
+					Separator{},
+					Action{
+						Text:        "E&xit",
+						OnTriggered: func() { mw.Close() },
+					},
 				},
 			},
-			PushButton{
-				Text: "마감일 안내받기",
-				OnClicked: func() {
-					day, name, count, countWithoutBlank := GetAlarmText()
-					Alarm(day, name, count, countWithoutBlank)
-					mainWindow.Close()
+			Menu{
+				Text: "&Help",
+				Items: []MenuItem{
+					Action{
+						AssignTo:    &showAboutBoxAction,
+						Text:        "About",
+						OnTriggered: mw.showAboutBoxAction_Triggered,
+					},
 				},
 			},
 		},
-	}.Run()
+		ToolBar: ToolBar{
+			ButtonStyle: ToolBarButtonImageBeforeText,
+			Items: []MenuItem{
+				ActionRef{&openAction},
+				Menu{
+					Text:  "New A",
+					Image: "../img/document-new.png",
+					Items: []MenuItem{
+						Action{
+							Text:        "A",
+							OnTriggered: mw.newAction_Triggered,
+						},
+						Action{
+							Text:        "B",
+							OnTriggered: mw.newAction_Triggered,
+						},
+						Action{
+							Text:        "C",
+							OnTriggered: mw.newAction_Triggered,
+						},
+					},
+					OnTriggered: mw.newAction_Triggered,
+				},
+				Separator{},
+				Menu{
+					Text:  "View",
+					Image: "../img/document-properties.png",
+					Items: []MenuItem{
+						Action{
+							Text:        "X",
+							OnTriggered: mw.changeViewAction_Triggered,
+						},
+						Action{
+							Text:        "Y",
+							OnTriggered: mw.changeViewAction_Triggered,
+						},
+						Action{
+							Text:        "Z",
+							OnTriggered: mw.changeViewAction_Triggered,
+						},
+					},
+				},
+				Separator{},
+				Action{
+					Text:        "Special",
+					Image:       "../img/system-shutdown.png",
+					Enabled:     Bind("isSpecialMode && enabledCB.Checked"),
+					OnTriggered: mw.specialAction_Triggered,
+				},
+			},
+		},
+		ContextMenuItems: []MenuItem{
+			ActionRef{&showAboutBoxAction},
+		},
+		MinSize: Size{300, 200},
+		Layout:  VBox{},
+		Children: []Widget{
+			CheckBox{
+				Name:    "enabledCB",
+				Text:    "Open / Special Enabled",
+				Checked: true,
+			},
+			CheckBox{
+				Name:    "openHiddenCB",
+				Text:    "Open Hidden",
+				Checked: true,
+			},
+			PushButton{
+				AssignTo: &toggleSpecialModePB,
+				Text:     "알림 보기",
+				OnClicked: func() {
+
+					day, name, count, countWithoutBlank := GetAlarmText()
+					Alarm(day, name, count, countWithoutBlank)
+
+				},
+			},
+		},
+	}.Create()); err != nil {
+		log.Fatal(err)
+	}
+
+	addRecentFileActions := func(texts ...string) {
+		for _, text := range texts {
+			a := walk.NewAction()
+			a.SetText(text)
+			a.Triggered().Attach(mw.openAction_Triggered)
+			recentMenu.Actions().Add(a)
+		}
+	}
+
+	addRecentFileActions("Foo", "Bar", "Baz")
+
+	mw.Run()
+
+	// var mainWindow *walk.MainWindow
+
+	// MainWindow{
+	// 	AssignTo: &mainWindow,
+	// 	Title:    "마감 안내기",
+	// 	MinSize:  Size{120, 50},
+	// 	Layout:   VBox{},
+	// 	Children: []Widget{
+
+	// 		PushButton{
+	// 			Text: "작업 등록하기",
+	// 			OnClicked: func() {
+	// 				mainWindow.Close()
+	// 				Fileupload()
+	// 			},
+	// 		},
+	// 		PushButton{
+	// 			Text: "마감일 안내받기",
+	// 			OnClicked: func() {
+	// 				day, name, count, countWithoutBlank := GetAlarmText()
+	// 				Alarm(day, name, count, countWithoutBlank)
+	// 				mainWindow.Close()
+	// 			},
+	// 		},
+	// 	},
+	// }.Run()
+}
+
+func (mw *MyMainWindow) openAction_Triggered() {
+	walk.MsgBox(mw, "Open", "Pretend to open a file...", walk.MsgBoxIconInformation)
+}
+
+func (mw *MyMainWindow) newAction_Triggered() {
+	walk.MsgBox(mw, "New", "Newing something up... or not.", walk.MsgBoxIconInformation)
+}
+
+func (mw *MyMainWindow) changeViewAction_Triggered() {
+	walk.MsgBox(mw, "Change View", "By now you may have guessed it. Nothing changed.", walk.MsgBoxIconInformation)
+}
+
+func (mw *MyMainWindow) showAboutBoxAction_Triggered() {
+	walk.MsgBox(mw, "About", "Walk Actions Example", walk.MsgBoxIconInformation)
+}
+
+func (mw *MyMainWindow) specialAction_Triggered() {
+	walk.MsgBox(mw, "Special", "Nothing to see here.", walk.MsgBoxIconInformation)
 }
