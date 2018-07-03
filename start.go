391175ef868b61308037230b24457e3c6d223787
hero0926 <esistkalt@naver.com>
2018-07-03 17:29:16 +0900

    기본기능 완료

--- start.go
+++ start.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"log"
-)
-
-import (
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 )
@@ -20,36 +16,15 @@ func main() {
 
 	mw := new(MyMainWindow)
 
-	var openAction, showAboutBoxAction, fileUploadAction *walk.Action
-	var recentMenu *walk.Menu
+	var showAboutBoxAction, fileUploadAction *walk.Action
+
 	//var toggleSpecialModePB *walk.PushButton
 
 	if err := (MainWindow{
 		AssignTo: &mw.MainWindow,
 		Title:    "마감 안내기",
 		MenuItems: []MenuItem{
-			Menu{
-				Text: "&File",
-				Items: []MenuItem{
-					Action{
-						AssignTo:    &openAction,
-						Text:        "&Open",
-						Enabled:     Bind("enabledCB.Checked"),
-						Visible:     Bind("!openHiddenCB.Checked"),
-						Shortcut:    Shortcut{walk.ModControl, walk.KeyO},
-						OnTriggered: mw.openAction_Triggered,
-					},
-					Menu{
-						AssignTo: &recentMenu,
-						Text:     "Recent",
-					},
-					Separator{},
-					Action{
-						Text:        "E&xit",
-						OnTriggered: func() { mw.Close() },
-					},
-				},
-			},
+
 			Menu{
 				Text: "&Help",
 				Items: []MenuItem{
@@ -87,7 +62,7 @@ func main() {
 			ActionRef{&showAboutBoxAction},
 		},
 
-		MinSize: Size{300, 200},
+		MinSize: Size{270, 150},
 		Layout:  VBox{},
 		Children: []Widget{
 			PushButton{
@@ -99,43 +74,19 @@ func main() {
 			},
 		},
 	}.Create()); err != nil {
-		log.Fatal(err)
+		walk.MsgBox(mw, "err", err.Error(), walk.MsgBoxIconInformation)
 	}
-
-	addRecentFileActions := func(texts ...string) {
-		for _, text := range texts {
-			a := walk.NewAction()
-			a.SetText(text)
-			a.Triggered().Attach(mw.openAction_Triggered)
-			recentMenu.Actions().Add(a)
-		}
-	}
-
-	addRecentFileActions("Foo", "Bar", "Baz")
 
 	mw.Run()
 }
 
-func (mw *MyMainWindow) openAction_Triggered() {
-	walk.MsgBox(mw, "Open", "Pretend to open a file...", walk.MsgBoxIconInformation)
-}
-
-func (mw *MyMainWindow) newAction_Triggered() {
-	walk.MsgBox(mw, "New", "Newing something up... or not.", walk.MsgBoxIconInformation)
-}
-
-func (mw *MyMainWindow) changeViewAction_Triggered() {
-	walk.MsgBox(mw, "Change View", "By now you may have guessed it. Nothing changed.", walk.MsgBoxIconInformation)
-}
-
 func (mw *MyMainWindow) showAboutBoxAction_Triggered() {
-	walk.MsgBox(mw, "About", "글 쓰시는 분들의 마감을 도와드립니다. 20180702 히어로 제작", walk.MsgBoxIconInformation)
+	walk.MsgBox(mw, "About", `글 쓰시는 분들의 마감을 도와드립니다.
+			20180703 히어로
+				github @hero0926
+		`, walk.MsgBoxIconInformation)
 }
 
 func (mw *MyMainWindow) fileUploadAction_Triggered() {
 	Fileupload()
-}
-
-func (mw *MyMainWindow) specialAction_Triggered() {
-	walk.MsgBox(mw, "Special", "Nothing to see here.", walk.MsgBoxIconInformation)
 }
